af6cbd71feaa66d5fb4c3e249001e0e0a91a599e
gandjustas <none@none>
2013-06-15 20:03:03 +0400

    Added Field with Validation sample

--- Definitions/clientforms.d.ts
+++ Definitions/clientforms.d.ts
@@ -1,1 +1,21 @@
-function SPFormControl_AppendValidationErrorMessage(nodeId:string, errorResult): void;
+function SPFormControl_AppendValidationErrorMessage(nodeId: string, errorResult): void;
+
+declare module SPClientForms {
+    declare module ClientValidation {
+        export class ValidationResult {
+            constructor(hasErrors: bool, errorMsg: string);
+        }
+
+        export class ValidatorSet {
+            public RegisterValidator(validator: IValidator);
+        }
+
+        export interface IValidator {
+            Validate(value: any): ValidationResult;
+        }
+
+        export class RequiredValidator implements IValidator {
+            Validate(value: any): ValidationResult;
+        }
+    }
+}
--- SPTypeScript/Definitions/SharePoint.d.ts
+++ SPTypeScript/Definitions/SharePoint.d.ts
@@ -180,7 +180,27 @@ declare class CalloutManager {
     static isAtLeastOneCalloutOn(): bool;
 }
 
-function SPFormControl_AppendValidationErrorMessage(nodeId:string, errorResult): void;
+function SPFormControl_AppendValidationErrorMessage(nodeId: string, errorResult): void;
+
+declare module SPClientForms {
+    declare module ClientValidation {
+        export class ValidationResult {
+            constructor(hasErrors: bool, errorMsg: string);
+        }
+
+        export class ValidatorSet {
+            public RegisterValidator(validator: IValidator);
+        }
+
+        export interface IValidator {
+            Validate(value: any): ValidationResult;
+        }
+
+        export class RequiredValidator implements IValidator {
+            Validate(value: any): ValidationResult;
+        }
+    }
+}
 
 declare module SPClientTemplates {
 
