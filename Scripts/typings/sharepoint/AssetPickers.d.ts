9bf67ba92b8117622995672b979782ab153a4e70
Андрей Милюк <amiluik@gmail.com>
2017-06-08 01:05:12 +0300

    Add AssetPickers.d.ts

---
+++ Scripts/typings/sharepoint/AssetPickers.d.ts
@@ -0,0 +1,21 @@
+﻿declare class AssetPickerConfig {
+    constructor(controlId: string);
+
+    AllowExternalUrls: boolean;
+    AssetTextClientID: string;
+    AssetType: string;
+    AssetUrlClientID: string;
+    CurrentWebBaseUrl: string;
+    DefaultAssetImageLocation: string;
+    DefaultAssetLocation: string;
+    DefaultToLastUsedLocation: boolean;
+    DisplayLookInSection: boolean;
+    OverrideDialogDesc: string;
+    OverrideDialogFeatures: object;
+    OverrideDialogImageUrl: string;
+    OverrideDialogTitle: string;
+    UseImageAssetPicker: boolean;
+    ReturnCallback: any;
+}
+
+declare function APD_LaunchAssetPicker(config: AssetPickerConfig, controlId: string): void;
