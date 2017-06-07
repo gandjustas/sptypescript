695218358755a78b05366869630563d2026e2443
Андрей Милюк <amiluik@gmail.com>
2017-06-08 01:21:00 +0300

    Add inplview.d.ts

---
+++ Scripts/typings/sharepoint/inplview.d.ts
@@ -0,0 +1,9 @@
+﻿// Type definitions for SharePoint JSOM 
+// Project: https://github.com/gandjustas/sptypescript
+// Definitions by: Stanislav Vyshchepan <http://blog.gandjustas.ru>, Andrey Markeev <http://markeev.com>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare var inplview: {
+    RefreshInplViewUrlByContext: (ctxParam: SPClientTemplates.RenderContext) => void;
+    HandleRefreshViewByContext: (ctxParam: SPClientTemplates.RenderContext, bClearPagingParam: boolean) => void;
+};
