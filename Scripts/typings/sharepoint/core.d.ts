8fb989b590613a20b6d857135b0a41b8d41b4db6
Андрей Милюк <amiluik@gmail.com>
2017-06-09 00:26:57 +0300

    Add core file

---
+++ Scripts/typings/sharepoint/core.d.ts
@@ -0,0 +1,6 @@
+﻿// Type definitions for SharePoint JSOM 
+// Project: https://github.com/gandjustas/sptypescript
+// Definitions by: Stanislav Vyshchepan <http://blog.gandjustas.ru>, Andrey Markeev <http://markeev.com>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare function FilterFieldV3(view: string, fieldName: string, fieldValue: string, selOption: number, queryString: string, bReturnUrl: boolean): void;
